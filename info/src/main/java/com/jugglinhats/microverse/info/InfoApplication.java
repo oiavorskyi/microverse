2011a9bc493327e61356c4384c642715252ed150
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2016-04-14 13:36:35 -0400

    Add basic resource and security rules

--- info/src/main/java/com/jugglinhats/microverse/info/InfoApplication.java
+++ info/src/main/java/com/jugglinhats/microverse/info/InfoApplication.java
@@ -2,11 +2,26 @@ package com.jugglinhats.microverse.info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
 public class InfoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InfoApplication.class, args);
-	}
+    @RequestMapping( "/info" )
+    public Map<String, Object> info() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+    public static void main( String[] args ) {
+        SpringApplication.run(InfoApplication.class, args);
+    }
 }
