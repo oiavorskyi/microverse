1a83f8761f5b19748c23c0af24af4983ed09790e
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2016-04-27 15:27:42 -0400

    Add basic UI Portal

--- ui/src/main/java/com/jugglinhats/microverse/ui/UIApplication.java
+++ ui/src/main/java/com/jugglinhats/microverse/ui/UIApplication.java
@@ -2,11 +2,26 @@ package com.jugglinhats.microverse.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+@Controller
 public class UIApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UIApplication.class, args);
 	}
+
+    @RequestMapping( "/" )
+    public String home( Model model, Principal principal ) {
+        model.addAttribute("username", principal.getName());
+        return "index";
+    }
+
 }
