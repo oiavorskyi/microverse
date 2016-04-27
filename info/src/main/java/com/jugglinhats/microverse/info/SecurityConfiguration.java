fb63747f30d6a2154c2a0d7d604d185d4cc7d77d
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2016-04-27 16:06:27 -0400

    Add support for retrieving of open info

--- info/src/main/java/com/jugglinhats/microverse/info/SecurityConfiguration.java
+++ info/src/main/java/com/jugglinhats/microverse/info/SecurityConfiguration.java
@@ -2,13 +2,11 @@ package com.jugglinhats.microverse.info;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
-import org.springframework.security.config.annotation.method.configuration
-        .EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration
-        .GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.expression
-        .OAuth2MethodSecurityExpressionHandler;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
 @EnableResourceServer
--- ui/src/main/java/com/jugglinhats/microverse/ui/UIApplication.java
+++ ui/src/main/java/com/jugglinhats/microverse/ui/UIApplication.java
@@ -1,8 +1,12 @@
 package com.jugglinhats.microverse.ui;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +22,30 @@ public class UIApplication {
 		SpringApplication.run(UIApplication.class, args);
 	}
 
+    @Autowired
+        private OAuth2RestTemplate restTemplate;
+
     @RequestMapping( "/" )
     public String home( Model model, Principal principal ) {
         model.addAttribute("username", principal.getName());
         return "index";
+    }
+
+    @RequestMapping("/open-info")
+    public String openInfo(Model model) {
+        final ResponseEntity<Info> info = restTemplate.getForEntity
+                ("http://localhost:8081/data", Info.class);
+        model.addAttribute("info", info.getBody());
+        return "info";
+    }
+
+    @Data
+    public static class Info {
+
+        private String type;
+        private String id;
+        private String content;
+
     }
 
 }
